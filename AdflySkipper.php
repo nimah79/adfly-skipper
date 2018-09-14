f99cf11516cc0f7ea31e75926dab54dbf0d038e3
Nima HeydariNasab <nima.heydari79@yahoo.com>
2018-09-14 20:37:47 +0000

    Apply fixes from StyleCI

--- AdflySkipper.php
+++ AdflySkipper.php
@@ -3,39 +3,41 @@
 /**
  * Simple class to skip adf.ly ads
  * By NimaH79
- * NimaH79.ir
+ * NimaH79.ir.
  */
-
-class AdflySkipper {
-
-	public static function bypass($url) {
+class AdflySkipper
+{
+    public static function bypass($url)
+    {
         $response = self::curl_get_contents($url);
-        if(preg_match('/ysmm = \'(.*?)\'/', $response, $ysmm)) {
+        if (preg_match('/ysmm = \'(.*?)\'/', $response, $ysmm)) {
             $ysmm = $ysmm[1];
+
             return self::decode($ysmm);
         }
+
         return false;
     }
 
-    private static function decode($ysmm) {
+    private static function decode($ysmm)
+    {
         $I = '';
         $X = '';
-        for($m = 0; $m < strlen($ysmm); $m++) {
-            if($m % 2 == 0) {
+        for ($m = 0; $m < strlen($ysmm); $m++) {
+            if ($m % 2 == 0) {
                 $I .= self::charAt($ysmm, $m);
-            }
-            else {
+            } else {
                 $X = self::charAt($ysmm, $m).$X;
             }
         }
         $ysmm = $I.$X;
         $U = str_split($ysmm);
-        for($m = 0; $m < count($U); $m++) {
-            if(ctype_digit($U[$m])) {
-                for($R = $m + 1; $R < count($U); $R++) {
-                    if(ctype_digit($U[$R])) {
+        for ($m = 0; $m < count($U); $m++) {
+            if (ctype_digit($U[$m])) {
+                for ($R = $m + 1; $R < count($U); $R++) {
+                    if (ctype_digit($U[$R])) {
                         $S = (int) $U[$m] ^ (int) $U[$R];
-                        if($S < 10) {
+                        if ($S < 10) {
                             $U[$m] = $S;
                         }
                         $m = $R;
@@ -48,20 +50,23 @@ class AdflySkipper {
         $ysmm = base64_decode($ysmm);
         $ysmm = substr($ysmm, 16);
         $ysmm = substr($ysmm, 0, -16);
+
         return $ysmm;
     }
 
-    private static function curl_get_contents($url) {
+    private static function curl_get_contents($url)
+    {
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         $response = curl_exec($ch);
         curl_close($ch);
+
         return $response;
     }
 
-    private static function charAt($str, $pos) {
-        return $str{$pos};
+    private static function charAt($str, $pos)
+    {
+        return $str[$pos];
     }
-
 }
